66d1ea08c486b71ede810f5f2f0c0a35ac884612
Anton Lafchiev <44170834+Toneca99@users.noreply.github.com>
2019-08-12 21:56:30 +0300

    Updates

---
+++ precache-manifest.2c865ab1b2fcf9b8ed69e72134da8155.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c72f734929a6b37556ddf4bbbe2f1f3",
+    "url": "/React-Practice-Applications/index.html"
+  },
+  {
+    "revision": "737674138c0e1bdabf94",
+    "url": "/React-Practice-Applications/static/css/main.44ad8dfa.chunk.css"
+  },
+  {
+    "revision": "d50a1c0c38de05d0086c",
+    "url": "/React-Practice-Applications/static/js/2.8c91b07f.chunk.js"
+  },
+  {
+    "revision": "737674138c0e1bdabf94",
+    "url": "/React-Practice-Applications/static/js/main.c212cd55.chunk.js"
+  },
+  {
+    "revision": "68eb49f9b20c6d83604d",
+    "url": "/React-Practice-Applications/static/js/runtime~main.a3e09a86.js"
+  }
+]);
